55452bf7aeebe7ab3acbb84c6bf5beccd7a9b8ec
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-08 13:12:44 -0400

    Test the fizzbuzz method of the Solver class

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -33,6 +33,7 @@ describe Solver do
       expect(result).to eq(expected_output)
     end
   end
+
   context 'reverse method' do
     it "should return the reverse of the string: 'hello'" do
       input = 'hello'
@@ -46,6 +47,44 @@ describe Solver do
       expected_output = 'repoleved'
       result = @solver.reverse(input)
       expect(result).to eq(expected_output)
+    end
+  end
+
+  context 'fizzbuzz method' do
+    it 'should return the word: fizzbuzz when the input is: 15' do
+      input = 15
+      expected_output = 'fizzbuzz'
+      expect(@solver.fizzbuzz(input)).to eq(expected_output)
+    end
+
+    it 'should return the word: fizzbuzz when the input is: 30' do
+      input = 30
+      expected_output = 'fizzbuzz'
+      expect(@solver.fizzbuzz(input)).to eq(expected_output)
+    end
+
+    it 'should return the word: fizz when the input is: 6' do
+      input = 6
+      expected_output = 'fizz'
+      expect(@solver.fizzbuzz(input)).to eq(expected_output)
+    end
+
+    it 'should return the word: buzz when the input is: 25' do
+      input = 25
+      expected_output = 'buzz'
+      expect(@solver.fizzbuzz(input)).to eq(expected_output)
+    end
+
+    it 'should return the string: 7 when the input is: 7' do
+      input = 7
+      expected_output = '7'
+      expect(@solver.fizzbuzz(input)).to eq(expected_output)
+    end
+
+    it 'should return the string: 13 when the input is: 13' do
+      input = 13
+      expected_output = '13'
+      expect(@solver.fizzbuzz(input)).to eq(expected_output)
     end
   end
 end
