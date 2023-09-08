9c1621b5ded06f11ecc064d7879dc95429a87f05
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-08 13:07:19 -0400

    Create the factorial method in the Solver class

--- solver.rb
+++ solver.rb
@@ -1,5 +1,9 @@
 class Solver
-  def initialize
-    puts 'Solver initialized'
+  def factorial(number)
+    raise 'Invalid input' if number.negative? # throws an exception when the number is negative
+
+    return 1 if number.zero? # base case
+
+    number * factorial(number - 1) # recursive call
   end
 end
