66746274518438197633c9785fd63c420acfd801
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-08 13:05:45 -0400

    Test the factorial method of the Solver class

---
+++ spec/solver_spec.rb
@@ -0,0 +1,36 @@
+require_relative '../solver'
+
+describe Solver do
+  before :each do
+    @solver = Solver.new
+  end
+
+  context 'factorial method' do
+    it 'should return the factorial of the number: 0' do
+      input = 0
+      expected_output = 1
+      result = @solver.factorial(input)
+      expect(result).to eq(expected_output)
+    end
+
+    it 'should return the factorial of the number: 10' do
+      input = 10
+      expected_output = 3_628_800
+      result = @solver.factorial(input)
+      expect(result).to eq(expected_output)
+    end
+
+    it 'should raise an exception when the input number is negative' do
+      input = -4
+      expected_output = 'Invalid input'
+      expect { @solver.factorial(input) }.to raise_error(expected_output)
+    end
+
+    it 'should return the factorial of the number: 3' do
+      input = 3
+      expected_output = 6
+      result = @solver.factorial(input)
+      expect(result).to eq(expected_output)
+    end
+  end
+end
